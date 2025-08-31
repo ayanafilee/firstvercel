415ab93d481649a708aff71c91ba3b6971cf9be6
Ayana File <ayanafileorg@gmail.com>
2025-08-31 12:07:24 +0300

    with pdfs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
-const page = () => {
+const Page = () => {
   return (
     <>
-    <div>my first vercel try</div>
-    <p>and this is updated version</p>
+      <div>my first vercel try</div>
+      <p>and this is updated version</p>
+
+      <a
+        href="https://drive.google.com/uc?export=download&id=1tABIceAq4dnmOwF_wtH1kFL0_lrjDs4e"
+        download="myfile.pdf"
+        className="bg-blue-500 text-white px-4 py-2 rounded-lg inline-block mt-4"
+      >
+        Download PDF
+      </a>
     </>
-    
   )
 }
 
-export default page
+export default Page
